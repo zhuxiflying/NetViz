e6ba1824a54b8eb01e9e032ed7d9284aa130bc93
zhuxiflying <zhuxiflying2012@gmail.com>
2020-05-07 22:20:05 -1000

    tsne layout data; data loading by d3 csv;

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { csv } from "d3";
 
-function App() {
-  return <div className="App">Hello!</div>;
-}
+const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    csv("./data/tsne_layout.csv").then((data) => {
+      setData(data);
+    });
+  }, []);
+
+  return <div className="App"></div>;
+};
 
 export default App;
